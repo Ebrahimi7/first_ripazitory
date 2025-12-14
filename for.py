9f8279b85a44e221e0cd807d14339d3857db58fa
Ebrahimi7 <145272051+Ebrahimi7@users.noreply.github.com>
2025-12-14 14:22:18 +0330

    test2

---
+++ for.py
@@ -0,0 +1,24 @@
+# a = int(input())
+
+# for i in range(1, (a+1)):
+#     print(" ")
+#     for j in range(1,11):
+#         print(j*i , end=" ")
+
+
+# for x in range(0,20):
+#     print("hello %i" %x)
+#     if x > 9:
+#         break
+
+# x = int(input())
+
+# if x % 2 == 0:
+#     i = 2
+# else :
+#     i = 1    
+
+# while i < x:
+#     print(i)
+#     i = i + 2
+
