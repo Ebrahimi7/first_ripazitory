e009e71c799d21eeb86ea584d9597453275642f0
Ebrahimi7 <145272051+Ebrahimi7@users.noreply.github.com>
2025-12-14 14:53:37 +0330

    file 5
    xxxxx

---
+++ Untitled-1.py
@@ -0,0 +1,67 @@
+# ghad = 150
+
+# if 110 <= ghad <= 150:
+#     print("ok")
+
+# d = int(input("fasele? : "))
+
+# if 1 < d <= 3:
+#     print("khiabon")
+# elif 3 < d <= 5:
+#     print("bus")
+# else:
+#     print("car")        
+
+# for x in range(0, 20):
+#     print(x)
+#     if x < 9 :
+#         break
+
+# x = 1234
+
+# for i in range(0,3):
+#     z = int(input("ramz: "))
+#     if z == x :
+#         print("ok")
+#         break
+#     else:
+#         print("false")
+
+
+# x = int(input("adad: "))
+
+# while x < 50:
+#     print(x)
+#     x = x + 1
+
+# a = int(input())
+# u = int(input())
+
+# def jam (a,u):
+#     return(a+u)
+
+# print(jam(a,u))
+
+# x = 10
+
+# def s(x):
+#     if x > 9 :
+#         return True
+    
+# if s(x) == True:
+#     print("ok")
+
+
+
+
+# x = 11
+
+# def c():
+#     global x
+#     x = 10
+#     print(x)
+
+# c()   
+
+# print(x)
+
